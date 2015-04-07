314f5bbd25d048bb2632cce9a0a387cba04e6a70
Dmitry Sigaev <Dmitry_Sigaev@epam.com>
2015-04-07 17:43:47 +0300

    add parseFlag  *	Parses a string to check line by a template. Converts string data to boolean  *	@method parseFlag  *	@param {String} flag the string to parse  *	@return {Bolean} type from the flag string

--- ctfile.js
+++ ctfile.js
@@ -1,4 +1,4 @@
-﻿/* ctfile.js
+/* ctfile.js
  * Copyright 2016 Dmitry Sigaev
  *
  * Released under the MIT license -- see MIT-LICENSE for details
@@ -28,6 +28,16 @@ CTFile.prototype.getVersion = function () {
 	return {
 		moduleVersion: '0.0.0'
 	};
+};
+
+/*
+ *	Parses a string to check line by a template. Converts string data to boolean
+ *	@method parseFlag
+ *	@param {String} flag the string to parse
+ *	@return {Bolean} type from the flag string
+ */
+var parseFlag = function (flag) {
+	return ((parseInt(flag) == 1) || /^\s*true\s*$/.test(flag))? true : false;
 };
 
 /*
